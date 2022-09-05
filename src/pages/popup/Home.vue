00b2f6dc798c734e28bce24f1f1f1cdac6097559
lishengyong <lishengyong@daojia-inc.com>
2022-09-05 18:48:51 +0800

    feat: 标签内容获取

--- src/pages/popup/Home.vue
+++ src/pages/popup/Home.vue
@@ -5,11 +5,41 @@
 </template>
 <script>
 
+import { ref } from 'vue'
+
 export default {
   setup(props) {
+
+    const bookmarkTreeNodes = ref({})
+
+    chrome.bookmarks.getTree((bookmarkTreeNodes) => {
+      console.log('bookmarkTreeNodes: ', bookmarkTreeNodes)
+      bookmarkTreeNodes.value = bookmarkTreeNodes
+    });
+
+    const remove = (id) => {
+      chrome.bookmarks.remove(id)
+    }
+
+    const create = (config) => {
+      chrome.bookmarks.create({
+        parentId: '',
+        title: '',
+        url: ''
+      })
+    }
+
+    const update = (id, config) => {
+      chrome.bookmarks.update(id, {
+        title: ''
+      })
+    }
     
     return {
-
+      bookmarkTreeNodes,
+      remove,
+      create,
+      update
     }
   }
 }
