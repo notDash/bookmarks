d4822006fd9a2ebdcb0afa1747bb661d4081c205
lishengyong <lishengyong@daojia-inc.com>
2022-09-14 14:25:57 +0800

    feat: 热门点击修改

--- src/components/HotTop5.vue
+++ src/components/HotTop5.vue
@@ -1,15 +1,22 @@
 <template>
     <div class="hot-top-5">
         <div v-for="(item, index) in searchHotItems" :key="index" class="bookmark-item">
-            <a :href="item.url">{{item.title}}</a>
+            <a :href="item.url">{{item.title}}</a><n-icon @click="() => onItemDel(item)" class="icon-delete" :size="16"><TrashOutline /></n-icon>
         </div>
     </div>
 </template>
 
 <script lang="ts">
 import { defineComponent, ref } from 'vue'
+import { TrashOutline } from '@vicons/ionicons5'
+import { NIcon } from "naive-ui"
+import formItemDark from 'naive-ui/es/form/styles/dark'
 
 export default defineComponent({
+    components: {
+        TrashOutline,
+        NIcon
+    },
     setup(props, context) {
         console.log(props)
 
@@ -28,18 +35,17 @@ export default defineComponent({
             let result = []
             if (searchhots) {
                 hotIds = JSON.parse(searchhots)
-                searchHotItems.value = hotIds
+               
                 // 遍历， 拿到点击次数最多的5个。暂未考虑性能
                 if (hotIds.length > limitLen) {
                     result = hotIds.sort(function(a: any, b: any) {
                         return b.times - a.times
                     }).slice(0, limitLen).map(function(item: any) {
-                        return item.id + ''
+                        return item
                     })
+                    searchHotItems.value = result
                 } else {
-                    result = hotIds.map(function(item: any) {
-                        return item.id + ''
-                    })
+                    searchHotItems.value = hotIds
                 }
             }
             console.log('result: .>>', result)
@@ -62,10 +68,21 @@ export default defineComponent({
 
         initSearchHot()
         
-
+        const onItemDel = (item: any) => {
+            const searchhots = localStorage.getItem('SEARCH_HOT_INS') || ''
+            const hotIns = JSON.parse(searchhots)
+            if (hotIns) {
+                const res = hotIns.filter((data: any) => {
+                    return data.id !== item.id
+                })
+                localStorage.setItem('SEARCH_HOT_INS', JSON.stringify(res))
+                initSearchHot()
+            }
+        }
 
 
         return {
+            onItemDel,
             getSearchHotIds,
             searchHotIds,
             searchHotItems,
@@ -74,3 +91,18 @@ export default defineComponent({
     },
 })
 </script>
+<style lang="less" scoped>
+.bookmark-item {
+    display: flex;
+    align-items: center;
+    border-bottom: solid 1px #eee;
+    padding: 6px 0;
+}
+.bookmark-item a {
+    margin-right: 10px;
+    text-decoration: none;
+}
+.icon-delete {
+    cursor: pointer;
+}
+</style>
